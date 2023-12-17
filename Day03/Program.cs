4f1194a6fc0d785c944299b3f02e1492ca83f367
Kamori310 <paul.serdack@web.de>
2023-12-17 15:48:14 +0100

    Refactoring

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-namespace Day03;
+﻿namespace Day03;
 
 public static class Day03 {
     public static int Main() {
@@ -29,8 +27,8 @@ public static class Day03 {
 }
 
 internal class SolutionPart1(string[] input) {
-    private readonly int _width = input[0].Length;
     private readonly int _height = input.Length;
+    private readonly int _width = input[0].Length;
 
     public int Calculation() {
         var solution = 0;
@@ -39,17 +37,22 @@ internal class SolutionPart1(string[] input) {
             var inNumber = false;
             var start = -1;
             for (var j = 0; j < _width; j++)
-                if (!inNumber && char.IsDigit(input[i][j])) {
-                    inNumber = true;
-                    start = j;
-                    number += input[i][j];
-                } else if (inNumber && char.IsDigit(input[i][j])) {
-                    number += input[i][j];
-                } else if (inNumber && !char.IsDigit(input[i][j])) {
-                    inNumber = false;
-                    var end = j - 1;
-                    solution += CheckNumber(number, i, start, end);
-                    number = "";
+                switch (inNumber) {
+                    case false when char.IsDigit(input[i][j]):
+                        inNumber = true;
+                        start = j;
+                        number += input[i][j];
+                        break;
+                    case true when char.IsDigit(input[i][j]):
+                        number += input[i][j];
+                        break;
+                    case true when !char.IsDigit(input[i][j]): {
+                        inNumber = false;
+                        var end = j - 1;
+                        solution += CheckNumber(number, i, start, end);
+                        number = "";
+                        break;
+                    }
                 }
 
             if (inNumber)
@@ -59,47 +62,46 @@ internal class SolutionPart1(string[] input) {
         return solution;
     }
 
-    private int CheckNumber(string number, int row, int startColumn, int endColumn)
-    {
-        for (var i = int.Max(0, row - 1); i < int.Min(_height, row + 2); i++) {
-            for (var j = int.Max(0, startColumn - 1); j < int.Min(_width, endColumn + 2); j++) {
-                if ("#$%&*+-/=@".Contains(input[i][j])) {
-                    return int.Parse(number);
-                }
-            }
-        }
+    private int CheckNumber(string number, int row, int startColumn, int endColumn) {
+        for (var i = int.Max(0, row - 1); i < int.Min(_height, row + 2); i++)
+        for (var j = int.Max(0, startColumn - 1); j < int.Min(_width, endColumn + 2); j++)
+            if ("#$%&*+-/=@".Contains(input[i][j]))
+                return int.Parse(number);
 
         return 0;
     }
 }
 
-class SolutionPart2(string[] input){
-    private readonly int _width = input[0].Length;
+internal class SolutionPart2(string[] input) {
     private readonly int _height = input.Length;
 
-    private readonly Dictionary<(int, int), List<int>> _possibleGears = new Dictionary<(int, int), List<int>>();
+    private readonly Dictionary<(int, int), List<int>> _possibleGears = new();
+    private readonly int _width = input[0].Length;
+
     public int Calculation() {
         for (var i = 0; i < _height; i++) {
             var number = "";
             var inNumber = false;
             var start = -1;
             for (var j = 0; j < _width; j++)
-                if (!inNumber && char.IsDigit(input[i][j])) {
-                    inNumber = true;
-                    start = j;
-                    number += input[i][j];
-                } else if (inNumber && char.IsDigit(input[i][j])) {
-                    number += input[i][j];
-                } else if (inNumber && !char.IsDigit(input[i][j])) {
-                    inNumber = false;
-                    var end = j - 1;
-                    CheckNumber(number, i, start, end);
-                    number = "";
+                switch (inNumber) {
+                    case false when char.IsDigit(input[i][j]):
+                        inNumber = true;
+                        start = j;
+                        number += input[i][j];
+                        break;
+                    case true when char.IsDigit(input[i][j]):
+                        number += input[i][j];
+                        break;
+                    case true when !char.IsDigit(input[i][j]): {
+                        inNumber = false;
+                        CheckNumber(number, i, start, j - 1);
+                        number = "";
+                        break;
+                    }
                 }
 
-            if (inNumber) {
-                CheckNumber(number, i, start, _width);
-            }
+            if (inNumber) CheckNumber(number, i, start, _width);
         }
 
         return _possibleGears
@@ -107,18 +109,14 @@ class SolutionPart2(string[] input){
             .Select(kvp => kvp.Value.Aggregate(1, (x, y) => x * y)).Sum();
     }
 
-    private void CheckNumber(string number, int row, int startColumn, int endColumn)
-    {
-        for (var i = int.Max(0, row - 1); i < int.Min(_height, row + 2); i++) {
-            for (var j = int.Max(0, startColumn - 1); j < int.Min(_width, endColumn + 2); j++) {
-                if ("*".Contains(input[i][j])) {
-                    if (_possibleGears.ContainsKey((i, j))) {
-                        _possibleGears[(i, j)].Add(int.Parse(number));
-                    } else {
-                        _possibleGears.Add((i, j), [int.Parse(number)]);
-                    }
-                }
+    private void CheckNumber(string number, int row, int startColumn, int endColumn) {
+        for (var i = int.Max(0, row - 1); i < int.Min(_height, row + 2); i++)
+        for (var j = int.Max(0, startColumn - 1); j < int.Min(_width, endColumn + 2); j++)
+            if ("*".Contains(input[i][j])) {
+                if (_possibleGears.ContainsKey((i, j)))
+                    _possibleGears[(i, j)].Add(int.Parse(number));
+                else
+                    _possibleGears.Add((i, j), [int.Parse(number)]);
             }
-        }
     }
 }
