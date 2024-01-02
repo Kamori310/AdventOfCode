056475393bbb1ff3594c90f2fa9db9de7e06e00c
Kamori310 <paul.serdack@web.de>
2024-01-02 12:53:37 +0100

    More Day 10 Part 1

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -44,47 +44,117 @@ internal class Part1 {
         }
 
         path[start.Row][start.Col] = 0;
-        char currentSymbole;
-        Direction movingDirection;
-        
-        var nextPositions = lines[start.Row][start.Col] switch {
-            '|' => []
-        }
-        while (nextPositions.All(position => position != start)) {
+        var currentSymbol = lines[start.Row][start.Col];
 
+        var moves = InitializeMoves(start, currentSymbol);
+
+        while (true) {
+            if (moves.All(move => ValidateMove(move, lines))) {
+                // Next position
+                // Next symbol
+                // Update path
+                // Next Moves
+                // Check if end is reached
+            } else {
+                return (false, path);
+            }
         }
 
     }
 
-    private bool ValidateMove(char previousSymbol, char currentSymbol, Direction direction) {
-        switch (direction) {
+    private static List<Move> InitializeMoves(Point start, char currentSymbol) {
+        List<Move> moves =
+            currentSymbol switch {
+                '|' => [
+                    new Move(0, start, Direction.Top),
+                    new Move(0, start, Direction.Bottom)
+                ],
+                '-' => [
+                    new Move(0, start, Direction.Left),
+                    new Move(0, start, Direction.Right)
+                ],
+                '7' => [
+                    new Move(0, start, Direction.Left),
+                    new Move(0, start, Direction.Bottom)
+                ],
+                'F' => [
+                    new Move(0, start, Direction.Right),
+                    new Move(0, start, Direction.Bottom)
+                ],
+                'L' => [
+                    new Move(0, start, Direction.Top),
+                    new Move(0, start, Direction.Right)
+                ],
+                'J' => [
+                    new Move(0, start, Direction.Top),
+                    new Move(0, start, Direction.Left)
+                ],
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        return moves;
+    }
+
+    private bool ValidateMove(Move move, List<string> lines) {
+        var currentSymbol = lines[move.CurrentPosition.Row][move.CurrentPosition.Col];
+        var nextPosition = CalculateNextPosition(move);
+
+        if (nextPosition.Row < 0 ||
+            nextPosition.Row >= lines.Count ||
+            nextPosition.Col < 0 ||
+            nextPosition.Col >= lines[0].Length) {
+            return false;
+        }
+
+        var nextSymbol = lines[nextPosition.Row][nextPosition.Col];
+
+        switch (move.Direction) {
             case Direction.Left:
-                if (previousSymbol is '7' or 'J' or '-' && currentSymbol is 'F' or 'L' or '-') {
+                if (currentSymbol is '7' or 'J' or '-' && nextSymbol is 'F' or 'L' or '-') {
                     return true;
                 }
+
                 break;
             case Direction.Top:
-                if (previousSymbol is '|' or 'J' or 'L' && currentSymbol is 'F' or '|' or '7') {
+                if (currentSymbol is '|' or 'J' or 'L' && nextSymbol is 'F' or '|' or '7') {
                     return true;
                 }
+
                 break;
             case Direction.Right:
-                if (previousSymbol is 'F' or 'L' or '-' && currentSymbol is '7' or 'J' or '-') {
+                if (currentSymbol is 'F' or 'L' or '-' && nextSymbol is '7' or 'J' or '-') {
                     return true;
                 }
+
                 break;
             case Direction.Bottom:
-                if (previousSymbol is 'F' or '|' or '7' && currentSymbol is '|' or 'J' or 'L') {
+                if (currentSymbol is 'F' or '|' or '7' && nextSymbol is '|' or 'J' or 'L') {
                     return true;
                 }
+
                 break;
         }
 
         return false;
     }
+
+    private static Point CalculateNextPosition(Move move) {
+        var nextPosition = move.Direction switch {
+            Direction.Left => move.CurrentPosition with { Col = move.CurrentPosition.Col - 1 },
+            Direction.Right => move.CurrentPosition with { Col = move.CurrentPosition.Col + 1 },
+            Direction.Top => move.CurrentPosition with { Row = move.CurrentPosition.Row + 1 },
+            Direction.Bottom => move.CurrentPosition with { Row = move.CurrentPosition.Row - 1 },
+        };
+        return nextPosition;
+    }
 }
 
 public record Point(int Row, int Col);
+
+internal record Move(
+    int MoveNumber,
+    Point CurrentPosition,
+    Direction Direction
+);
 
 public enum Direction {
     Left,
